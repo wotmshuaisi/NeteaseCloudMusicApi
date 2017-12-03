ccaf374c8d6abb85312e68ca4e27ed5a65c01e73
小莹草 <rem486@qq.com>
2017-12-03 18:22:57 +0800

    1.添加编译运行的脚本 2.修改ui 3.好困,昨晚舍友的朋友来,玩的很深夜,被迫修仙啊惨

--- routers/commentsRouter_.go
+++ routers/commentsRouter_.go
@@ -17,16 +17,16 @@ func init() {
 
 	beego.GlobalControllerRouter["github.com/ActingCute/NeteaseCloudMusicApi/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/ActingCute/NeteaseCloudMusicApi/controllers:UserController"],
 		beego.ControllerComments{
-			Method: "Playlist",
-			Router: `/playlist`,
+			Method: "Detail",
+			Router: `/detail`,
 			AllowHTTPMethods: []string{"get"},
 			MethodParams: param.Make(),
 			Params: nil})
 
 	beego.GlobalControllerRouter["github.com/ActingCute/NeteaseCloudMusicApi/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/ActingCute/NeteaseCloudMusicApi/controllers:UserController"],
 		beego.ControllerComments{
-			Method: "Detail",
-			Router: `/detail`,
+			Method: "Playlist",
+			Router: `/playlist`,
 			AllowHTTPMethods: []string{"get"},
 			MethodParams: param.Make(),
 			Params: nil})
